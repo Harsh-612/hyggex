167bf3088f9474635fc6bbdbc91a29ea7430ad91
Harsh-612 <hchaturvedi126@gmail.com>
2024-01-16 19:42:46 +0530

    minor ui changes

--- src/components/AddressBar.tsx
+++ src/components/AddressBar.tsx
@@ -3,8 +3,8 @@ import "remixicon/fonts/remixicon.css";
 const AddressBar: React.FC = () => {
   return (
     <div className="text-base text-gray-500 flex mt-6">
-      <i className="ri-home-4-line"></i>
-      {">"}Flashcard{">"}Mathematics{">"}
+      <i className="ri-home-4-line"></i>&nbsp;
+      {" > "}Flashcard&nbsp;{" > "}&nbsp;Mathematics{" > "}&nbsp;
       <div className="font-medium text-gray-800">Relations and Functions</div>
     </div>
   );
